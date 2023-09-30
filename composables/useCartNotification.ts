9ca5d6785a34ccd86b8342880ede222247743e0a
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-30 23:00:34 +0700

    Initial commit

---
+++ composables/useCartNotification.ts
@@ -0,0 +1,39 @@
+import { CartErrors } from "@shopware-pwa/types";
+
+const successCodes = ["promotion-discount-added"];
+
+export type useCartNotificationReturn = {
+  codeErrorsNotification(): void;
+};
+
+/**
+ * UI composable
+ *
+ * @returns
+ */
+export function useCartNotification(): useCartNotificationReturn {
+  const { pushError, pushSuccess } = useNotifications();
+  const { consumeCartErrors } = useCart();
+
+  /**
+   * Get cart error and display
+   *
+   * @returns {void}
+   */
+  const codeErrorsNotification = () => {
+    const errors: CartErrors | null = consumeCartErrors();
+    if (!errors) return;
+
+    Object.keys(errors).forEach((element) => {
+      if (successCodes.includes(errors[element].messageKey)) {
+        pushSuccess(errors[element].message);
+      } else {
+        pushError(errors[element].message);
+      }
+    });
+  };
+
+  return {
+    codeErrorsNotification,
+  };
+}
